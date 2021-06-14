f49f5eaf14f886f9130e2296e29101f8b5c00b8b
Adilson Ortolan <adortolan@gmail.com>
2021-06-14 20:35:38 -0300

    Primeiro commit CI

---
+++ calc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    sum: function(n1, n2){
+        return n1 + n2;
+    },
+    sub: function(n1, n2){
+        return n1 - n2;
+    },
+    mult: function(n1, n2){
+        return n1 * n2;
+    },
+    div: function(n1, n2){
+        return n1 / n2;
+    },
+};
---
+++ test.js
@@ -0,0 +1,10 @@
+var calcModule = require('./calc');
+var chai = require('chai');
+var expect = chai.expect;
+describe('calc test', function() {
+    it('should sum return 4', function(done) {
+      var result = calcModule.sum(2, 2);
+      expect(result).to.equal(4);
+      done();
+    });
+  });
