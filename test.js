1da0c3fc9285973b99d9424d61d2eaecbdedeb75
Adilson Ortolan <adortolan@gmail.com>
2021-06-16 17:15:05 -0300

    CI - com erro

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ var expect = chai.expect;
 describe('calc test', function() {
     it('should sum return 4', function(done) {
       var result = calcModule.sum(2, 2);
-      expect(result).to.equal(4);
+      expect(result).to.equal(5);
       done();
     });
   });
