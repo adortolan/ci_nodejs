3ee0d5188e8d370b1230ae3ecb9aa6bd4b7e259b
Adilson Ortolan <adortolan@gmail.com>
2021-06-16 17:21:15 -0300

    CI -Workflow

--- test.js
+++ test.js
@@ -4,8 +4,8 @@ var expect = chai.expect;
 describe('calc test', function() {
     it('should sum return 4', function(done) {
       var result = calcModule.sum(2, 2);
-      expect(result).to.equal(5);
+      expect(result).to.equal(4);
       done();
     });
-  });
+  }); 
  
